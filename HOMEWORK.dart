1d0e9548f9bf9f8937a2eda66547ed0d0596708a
sebnem65 <162494088+sebnem65@users.noreply.github.com>
2024-10-05 19:14:27 +0300

    Add files via upload

---
+++ HOMEWORK.dart
@@ -0,0 +1,55 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: type=lint
+
+//                             1.ödev
+void main() {
+  List<int> numbers = [5, 10, 15, 20, 25];
+  int count = 0;
+
+  for (int number in numbers) {
+    if (number % 2 == 0) {
+      count++;
+    }
+  }
+  print("çift sayilarin sayisi: $count");
+}
+
+                             
+
+
+
+ //                                   2ODEV
+void main() {
+  Map<String, int> notlar = {'Ali': 4, 'Veli': 6, 'Ahmet': 8};
+  int toplam = 0;
+
+  for (int not int notlar.values){
+    toplam += not;
+  } 
+if (toplam > 10 ){
+  print("Büyük");
+
+}
+else{
+  print("Küçük");
+ }
+}
+
+//                                   3ODEV
+void main(){
+  Set<int> sayiKumesi = {1, 5, 9, 14, 10};
+
+
+  if (sayiKumesi.contains(10)){
+    print("10 bulundu");
+
+  }
+  else{
+    print("10 bulunamadi");
+  }
+}
+  
